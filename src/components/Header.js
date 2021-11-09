70bde678c38544069793de7269c646cb72fa9c6b
Zulma Rocio <zrmartinezg@gmail.com>
2021-11-09 12:06:20 -0500

    Add active class

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import planet from '../assets/planet.png';
 
@@ -11,15 +11,15 @@ const Header = () => (
         <h1 className="title  col-8">Space Travelers Hub</h1>
       </div>
       <div className="col-4">
-        <Link to="/">
+        <NavLink to="/" activeClassName="active-link">
           <span className="rockets-nav mx-3">Rockets</span>
-        </Link>
-        <Link to="/missions">
+        </NavLink>
+        <NavLink to="/missions" activeClassName="active-link">
           <span className="missions-nav mx-3">Missions</span>
-        </Link>
-        <Link to="/Profile">
+        </NavLink>
+        <NavLink to="/Profile" activeClassName="active-link">
           <span className="profile-nav mx-3">Profile</span>
-        </Link>
+        </NavLink>
       </div>
     </nav>
   </div>
