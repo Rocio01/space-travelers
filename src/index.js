827d689cb0a4caa4f7e6a1fb2dcbb25a270d57a5
Zulma Rocio <zrmartinezg@gmail.com>
2021-11-08 21:02:23 -0500

    Add Provider in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {Provider} from 'react-redux';
+import {store} from '../src/redux/configureStore';
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider> 
   </React.StrictMode>,
   document.getElementById('root')
 );
