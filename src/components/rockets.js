321bba404e05896924261c62bdb397dc2e993ce7
Amalcxc <amalcxirsi@gmail.com>
2021-11-11 00:49:17 +0300

    reserved

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -1,11 +1,19 @@
 /* eslint-disable camelcase */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { setReserved } from '../redux/rockets/rockets';
 
 const Rockets = (props) => {
   const {
-    id, rocket_name, description, flickr_images,
+    id, rocket_name, description, flickr_images, reserved = false,
   } = props;
+
+  const dispatch = useDispatch();
+
+  const newReservetion = () => {
+    dispatch(setReserved(id));
+  };
 
   return (
     <div key={id} className="row m-4">
@@ -14,7 +22,9 @@ const Rockets = (props) => {
       </div>
       <div className="col-9">
         <h5>{rocket_name}</h5>
+        <span>{reserved}</span>
         <p>{description}</p>
+        <button type="button" onClick={newReservetion} className="btn btn-primary">Reserved Rocket</button>
       </div>
 
     </div>
@@ -26,6 +36,7 @@ Rockets.propTypes = {
   rocket_name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   flickr_images: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default Rockets;
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const SET_ROCKETS = 'space-travelers/rockets/SET_ROCKETS';
+const SET_RESERVED = 'SET_RESERVED';
 const URL = 'https://api.spacexdata.com/v3/rockets';
 const initialState = {
   rockets: [],
@@ -10,6 +11,14 @@ const rocketsReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_ROCKETS:
       return { ...state, rockets: action.payload };
+    case SET_RESERVED: {
+      const newState = state.rockets.map((rocket) => {
+        if (rocket.id !== action.id) return rocket;
+        return { ...rocket, reserved: true };
+      });
+      return newState;
+    }
+
     default:
       return state;
   }
@@ -18,6 +27,11 @@ const rocketsReducer = (state = initialState, action) => {
 const setRockets = (payload) => ({
   type: SET_ROCKETS,
   payload,
+});
+
+export const setReserved = (id) => ({
+  type: SET_RESERVED,
+  id,
 });
 
 export const fetchRockets = (dispatch) => {
