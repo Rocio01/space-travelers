f9ea9927bee4311734dc301981399c86dcb63aba
Amalcxc <amalcxirsi@gmail.com>
2021-11-11 14:17:25 +0300

    reserved

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -26,11 +26,11 @@ const Rockets = (props) => {
       </div>
       <div className="col-9">
         <h5>{rocket_name}</h5>
-        <span className={reserved === true ? 'btn-info span-mission' : 'btn-secondary span-mission'}>
+        <span className={reserved === true ? 'btn btn-primary' : 'btn'}>
           { reserved === true ? 'Reserved' : ''}
         </span>
         <p>{description}</p>
-        <button type="button" className={reserved === true ? 'btn btn-outline-danger mission-button' : 'btn btn-outline-dark mission-button'} onClick={reserved === true ? newReservetion : cancelReservetion}>{reserved === true ? 'Cancel Booking' : 'reserved rocket'}</button>
+        <button type="button" className={reserved === true ? 'btn-info ' : 'btn btn-primary'} onClick={reserved === true ? newReservetion : cancelReservetion}>{reserved === true ? 'Cancel Booking' : 'reserved rocket'}</button>
       </div>
 
     </div>
