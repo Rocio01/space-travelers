f4c23f651d403aaa09300cc37b3aabb255f89fb1
Zulma Rocio <zrmartinezg@gmail.com>
2021-11-10 22:58:31 -0500

    Add titles in profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,9 +3,14 @@ import ProfileMisions from './ProfileMisions';
 
 function Profile() {
   return (
-    <div>
-      <ProfileMisions />
-      ;
+    <div className="row m-5 p-3">
+      <div className="missions-div col-5 m-1 p-3">
+        <h2> My Missions</h2>
+        <ProfileMisions />
+      </div>
+      <div className="rockets-div col-5 m-1 p-3">
+        <h2> My Rockets </h2>
+      </div>
     </div>
   );
 }
