a4badc7b8d37a9a7d5f143cb71a902f0890b50ec
Zulma Rocio <zrmartinezg@gmail.com>
2021-11-12 11:34:30 -0500

    Add tests for missions to test the correct status display

--- src/tests/Mission.test.js
+++ src/tests/Mission.test.js
@@ -1,7 +1,10 @@
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Mission from '../components/Mission';
 import store from '../redux/configureStore';
+import MissionMock from './mocks/MissionMock';
 
 test('matches Mission component snapshot', () => {
   const tree = renderer.create(
@@ -10,4 +13,44 @@ test('matches Mission component snapshot', () => {
     </Provider>,
   ).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+
+
+test('should render Mission component correctly', () => {
+  const component = render(
+    <MissionMock
+      mission_id="TestId"
+      mission_name="Mission one"
+      description="description one"
+      status={false}
+    />,
+  );
+  expect(component.container).toHaveTextContent('Mission one');
+  expect(component.container).toHaveTextContent('description one');
+});
+
+
+test('If status = false innertext = NOT A MEMBER', () => {
+  const component = render(
+    <MissionMock
+      mission_id="TestId"
+      mission_name="Mission one"
+      description="description one"
+      status={false}
+    />,
+  );
+  expect(component.container).toHaveTextContent('NOT A MEMBER');
+});
+
+test('If status = true innertext = Active member', () => {
+  const component = render(
+    <MissionMock
+      mission_id="TestId"
+      mission_name="Mission two"
+      description="description two"
+      status={true}
+    />,
+  );
+  expect(component.container).toHaveTextContent('Active member');
 });
--- src/tests/MissionsContainer.test.js
+++ src/tests/MissionsContainer.test.js
@@ -1,21 +1,11 @@
 import React from 'react';
-//  import { screen, render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import missionsReducer from '../redux/missions/missions';
 import '@testing-library/jest-dom/extend-expect';
 import MissionsContainer from '../components/MissionsContainer';
 import store from '../redux/configureStore';
-
-// describe('Missions tests', () => {
-//   it('Missions page matches snapshot', async () => {
-//     await store.dispatch(fetchMissions());
-//     const missionsPage = renderer
-//       .create(<Provider store={store}><MissionsContainer /></Provider>)
-//       .toJSON();
-//     expect(missionsPage).toMatchSnapshot();
-//   });
-// });
 
 describe('missions', () => {
   test('matches MissionsContainer component snapshot', () => {
